7e972cd8f718b618c3bdfeb63c3995124b21306f
hanhhong.nguyen <hanhhong.nguyen@isobar.com>
2022-03-21 22:25:45 +0700

    Updates

---
+++ precache-manifest.210a020a109c8486dc7d203d56c78e54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7afb3ca2db11ca83c3e8",
+    "url": "/knowledge/static/css/main.5e19f604.chunk.css"
+  },
+  {
+    "revision": "7afb3ca2db11ca83c3e8",
+    "url": "/knowledge/static/js/main.7476b10a.chunk.js"
+  },
+  {
+    "revision": "c44a7f9edc475fa3fc98",
+    "url": "/knowledge/static/js/runtime~main.627c66ea.js"
+  },
+  {
+    "revision": "1ca69d0d4f5cdd47b5f9",
+    "url": "/knowledge/static/js/2.099f38da.chunk.js"
+  },
+  {
+    "revision": "01fc249a99330c5e752c8970901c128b",
+    "url": "/knowledge/static/media/banner.01fc249a.jpg"
+  },
+  {
+    "revision": "4cc093ff266f939abba50126c2efefdc",
+    "url": "/knowledge/static/media/banner-header.4cc093ff.png"
+  },
+  {
+    "revision": "9c831e3da904e46ae4afd38542e6005b",
+    "url": "/knowledge/index.html"
+  }
+];
