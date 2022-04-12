eaf557ed69becafeba78328f1cc4d68e4756e9d2
hanhhong.nguyen <hanhhong.nguyen@isobar.com>
2022-04-13 02:26:30 +0700

    Updates

--- precache-manifest.84077d6aea91ec5f6da6dc917451bf0d.js
+++ precache-manifest.84077d6aea91ec5f6da6dc917451bf0d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "2e7ffa18b293867806bf",
+    "revision": "f49a008ebd7014c7a27b",
     "url": "/knowledge/static/css/main.92304c00.chunk.css"
   },
   {
-    "revision": "2e7ffa18b293867806bf",
-    "url": "/knowledge/static/js/main.34535fe9.chunk.js"
+    "revision": "f49a008ebd7014c7a27b",
+    "url": "/knowledge/static/js/main.50bf32f8.chunk.js"
   },
   {
     "revision": "c44a7f9edc475fa3fc98",
     "url": "/knowledge/static/js/runtime~main.627c66ea.js"
   },
   {
-    "revision": "b0fb7c8ba27b77ea175e",
-    "url": "/knowledge/static/js/2.15f0997c.chunk.js"
+    "revision": "a6c96bf52240f51dd892",
+    "url": "/knowledge/static/js/2.44f90d53.chunk.js"
   },
   {
     "revision": "01fc249a99330c5e752c8970901c128b",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/knowledge/static/media/banner-header.4cc093ff.png"
   },
   {
-    "revision": "45cd1826b03db51084b6279a3535c082",
+    "revision": "7ab8001de86698fd96ec9ad4684a48c2",
     "url": "/knowledge/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/knowledge/precache-manifest.90996fd91d144e3cb8d2a579b0c66530.js"
+  "/knowledge/precache-manifest.84077d6aea91ec5f6da6dc917451bf0d.js"
 );
 
 workbox.clientsClaim();
